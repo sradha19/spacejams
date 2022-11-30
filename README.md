e6c56186979139b830288aad40b548b53b35c7bc
SumanBR321 <71173805+SumanBR321@users.noreply.github.com>
2022-11-30 20:44:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# spacejams
+In this space game we have enemies at the top of the screen; A player at the bottom of the screen; It is a simple game created in Javascript & Html.
+
+It's fun to play. Do try it out. Enjoy playing...!
---
+++ player.js
@@ -0,0 +1,76 @@
+export default class player {
+  rightPressed = false;
+  leftPressed = false;
+  shootPressed = false;
+
+  constructor(canvas, velocity, bulletController) {
+    this.canvas = canvas;
+    this.velocity = velocity;
+    this.bulletController = bulletController;
+
+    this.x = this.canvas.width / 2;
+    this.y = this.canvas.height - 200;
+    this.width = 200;
+    this.height = 140;
+    this.image = new Image();
+    this.image.src = "player.png";
+    document.addEventListener("keydown", this.keydown);
+    document.addEventListener("keyup", this.keyup);
+  }
+
+  draw(ctx) {
+    if (this.shootPressed) {
+      this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
+    }
+    this.move();
+    this.collideWithWalls();
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  collideWithWalls() {
+    //left
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    //right
+    if (this.x > this.canvas.width - this.width) {
+      this.x = this.canvas.width - this.width;
+    }
+  }
+
+  move() {
+    if (this.rightPressed) {
+      this.x =this.x + 4*this.velocity;
+    } else if (this.leftPressed) {
+      this.x = this.x - 5*this.velocity;
+    }
+  }
+
+  keydown = (event) => {
+    if (event.code == "ArrowRight") {
+      this.rightPressed = true;
+    }
+    if (event.code == "ArrowLeft") {
+      this.leftPressed = true;
+    }
+    if (event.code == "Space") {
+      this.shootPressed = true;
+    }
+    // trying to reset the game
+    if(event.code == "Enter"){
+      let isgameOver = false;
+    }
+  };
+
+  keyup = (event) => {
+    if (event.code == "ArrowRight") {
+      this.rightPressed = false;
+    }
+    if (event.code == "ArrowLeft") {
+      this.leftPressed = false;
+    }
+    if (event.code == "Space") {
+      this.shootPressed = false;
+    }
+  };
+}
